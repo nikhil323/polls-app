9fc9e0ca61a083de38c75bd2471f17484eed79c2
nikhil323 <nikhil323mahara@gmail.com>
2024-12-04 17:58:50 +0545

    feat: added views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render,get_object_or_404
 
+from .models import Question
 
-def index(request, **kwargs):
-    print("========>", request, kwargs)
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request):
+    latest_qsn_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_qsn_list
+    }
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": q})
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s" %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
