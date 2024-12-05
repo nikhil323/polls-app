e1122cf1f4b18397242aaf44fc2bc539bf4cc8c1
nikhil323 <nikhil323mahara@gmail.com>
2024-12-05 13:51:34 +0545

    updated django admin templates

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra= 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Questions", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]})
+
+    ]
+    inlines=[ChoiceInline]
+    list_display =["question_text", "pub_date","was_published_recently"]
+    list_filter = ["pub_date"] #shows filter to the right side by published date
+    search_fields = ["question_text"] #creates a search field to the top by question text
+    # fields = ["pub_date", "question_text"]
+
+admin.site.register(Question, QuestionAdmin)
